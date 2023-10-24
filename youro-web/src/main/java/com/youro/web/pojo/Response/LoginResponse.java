e2294047ebf6fc6ee4c234e271bd5437eba5e69a
Abhigna-Tumati <abhigna.tumati@gmail.com>
2023-10-23 23:13:10 -0400

    login response obj change

--- youro-web/src/main/java/com/youro/web/pojo/Response/LoginResponse.java
+++ youro-web/src/main/java/com/youro/web/pojo/Response/LoginResponse.java
@@ -2,26 +2,15 @@ package com.youro.web.pojo.Response;
 
 public class LoginResponse {
     private String token;
-
-    private long expiresIn;
+    
+    public LoginResponse(String token, long expiresIn) {
+		super();
+		this.token = token;
+	}
 
     public String getToken() {
         return token;
     }
-
-	public LoginResponse(String token, long expiresIn) {
-		super();
-		this.token = token;
-		this.expiresIn = expiresIn;
-	}
-
-	public long getExpiresIn() {
-		return expiresIn;
-	}
-
-	public void setExpiresIn(long expiresIn) {
-		this.expiresIn = expiresIn;
-	}
 
 	public void setToken(String token) {
 		this.token = token;
