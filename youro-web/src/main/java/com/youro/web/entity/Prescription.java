6851325d74f2304ae2ea0752cd4900948d03dcb9
mandavaravi <mandavaravindra1998@gmail.com>
2023-10-27 12:46:39 -0400

    get all prescriptions in admin controller

--- youro-web/src/main/java/com/youro/web/entity/Prescription.java
+++ youro-web/src/main/java/com/youro/web/entity/Prescription.java
@@ -2,16 +2,14 @@ package com.youro.web.entity;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "prescription")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Prescription {
