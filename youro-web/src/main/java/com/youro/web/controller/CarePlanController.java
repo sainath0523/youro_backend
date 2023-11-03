10ad7dc84304c9e51c79a0445c6dbbcbfb623559
Abhigna-Tumati <abhigna.tumati@gmail.com>
2023-11-02 20:35:36 -0400

    careplan results api's

--- youro-web/src/main/java/com/youro/web/controller/CarePlanController.java
+++ youro-web/src/main/java/com/youro/web/controller/CarePlanController.java
@@ -5,7 +5,11 @@ import com.youro.web.pojo.Request.SaveCheckListRequest;
 import com.youro.web.pojo.Request.SaveNotesRequest;
 import com.youro.web.pojo.Request.UpdateResultDetailsRequest;
 import com.youro.web.pojo.Response.*;
+import com.youro.web.service.CarePlanService;
+
 import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +21,10 @@ import java.util.List;
 @CrossOrigin(origins ="*")
 @RequestMapping("/youro/api/v1/")
 public class CarePlanController {
-
+	
+	@Autowired
+    CarePlanService carePlanService;
+	
     @PostMapping("/saveCheckList")
     public BasicResponse saveCheckList(@RequestBody @Valid SaveCheckListRequest requestBody) {
         return new BasicResponse();
@@ -27,22 +34,17 @@ public class CarePlanController {
     public List<GetCheckListResponse> getCheckList(@PathVariable("uId") int uId) {
         return new ArrayList<GetCheckListResponse>();
     }
-
+    
     @PostMapping("/uploadResults")
-    public List<UploadResultsResponse> uploadResults(@RequestParam("files") MultipartFile[] files) {
-        return new ArrayList<>();
+    public List<byte[]> uploadResults(@RequestParam("files") MultipartFile[] files, @RequestParam("patientId") int patientId) {
+    	carePlanService.uploadResults(files, patientId);
+    	return carePlanService.getResults(patientId);
     }
-
-    @PostMapping("/updateResultsDetails")
-    public BasicResponse updateResultsDetails(@RequestBody @Valid UpdateResultDetailsRequest requestBody) {
-        return new BasicResponse();
+    
+    @GetMapping("/getResults/{patientId}")
+    public List<byte[]> getResults(@PathVariable("patientId") int patientId) {
+        return carePlanService.getResults(patientId);
     }
-
-    @GetMapping("/getResults/{apptId}")
-    public List<byte[]> getResults(@PathVariable("apptId") int apptId) {
-        return new ArrayList<>();
-    }
-
 
     @PostMapping("/saveNotes")
     public BasicResponse saveNotes(@RequestBody @Valid SaveNotesRequest requestBody) {
---
+++ youro-web/src/main/java/com/youro/web/service/CarePlanService.java
@@ -0,0 +1,142 @@
+package com.youro.web.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.IOUtils;
+import com.youro.web.bucket.BucketName;
+import com.youro.web.entity.Results;
+import com.youro.web.entity.User;
+import com.youro.web.exception.CustomException;
+import com.youro.web.pojo.Response.BasicResponse;
+import com.youro.web.repository.ResultsRepository;
+import com.youro.web.repository.UserRepository;
+import com.youro.web.utils.HelpUtils;
+
+@Service
+public class CarePlanService {
+	
+	
+	@Autowired
+	AmazonS3 s3;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	ResultsRepository resultsRepository;
+
+	public BasicResponse uploadResults(MultipartFile[] files, int patientId) {
+		
+		if(files.length == 0)
+			throw new IllegalStateException("No files to upload");
+		
+		try {
+			String bucketName = String.format("%s", BucketName.PROFILE_IMAGE.getBucketName());
+			String rootPath = String.format("%s", Integer.toString(patientId));
+			this.save(bucketName, rootPath, files);
+			this.saveResultsUrl(patientId, bucketName, rootPath);
+			return new BasicResponse("Files uploaded to s3 successfully");
+		}
+		catch(IOException e) {
+			throw new IllegalStateException("Failed to store file to s3", e); 
+		}
+		
+	}
+
+	public void save(String bucketName, String rootPath, MultipartFile[] files) throws IOException {
+		try {
+			for (MultipartFile mFile: files) {
+				File file = convertFile(mFile);
+				String filePath = String.format("%s/%s", rootPath, file.getName());
+				s3.putObject(bucketName, filePath, file);
+			}
+			System.out.println("succesfully uploaded files");			
+		}
+		catch(AmazonServiceException e){
+			throw new CustomException("Failed to upload files to S3");
+		}
+	}
+	
+	private void saveResultsUrl(int patientId, String bucketName, String rootPath) {
+		String results_url = String.format("%s/%s", bucketName, rootPath);
+
+		Optional<User> user = userRepository.findByUserId(patientId);
+		if(user.isEmpty()) {
+            throw new CustomException("No user found with id: " + patientId);
+		}
+		Optional<Results> results = resultsRepository.findByPatientId(user.get());
+		if(results.isEmpty()) {
+			Results res = new Results();
+			res.setPatientId(HelpUtils.getUser(patientId));
+			res.setResults_url(results_url);
+			resultsRepository.save(res);		
+		}
+		else {
+			Results res = results.get();
+            res.results_url = results_url;
+            resultsRepository.save(res);
+		}
+				
+	}
+	
+	public static File convertFile(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
+	}
+
+	public List<byte[]> getResults(int patientId) {
+		Optional<User> user = userRepository.findByUserId(patientId);
+		if(user.isEmpty()) {
+            throw new CustomException("No user found with id: " + patientId);
+		}
+		Optional<Results> results = resultsRepository.findByPatientId(user.get());
+		if(results.isEmpty()) {
+            throw new CustomException("No results for patient: " + patientId);
+		}
+		String results_url = results.get().getResults_url();	
+		return getResultsFromS3(results_url);
+	}
+
+	private List<byte[]> getResultsFromS3(String results_url) {
+		try {
+			String[] parts = results_url.split("/");
+			String bucketName = parts[0];
+		    String rootPath = parts[1];
+		    
+	        List<byte[]> resultList = new ArrayList<>();
+	        ObjectListing objectListing = s3.listObjects(bucketName, rootPath);
+	        List<S3ObjectSummary> objectSummaries = objectListing.getObjectSummaries();
+	        
+	        for (S3ObjectSummary objectSummary : objectSummaries) {
+	            S3Object obj = s3.getObject(bucketName, objectSummary.getKey());
+	            S3ObjectInputStream inputStream = obj.getObjectContent();
+	            byte[] data = IOUtils.toByteArray(inputStream);
+                resultList.add(data);
+	        }
+	        return resultList;			
+		}
+		catch(AmazonServiceException | IOException e) {
+			throw new CustomException("Failed to get files from S3");
+		}
+	}
+
+}
